0fe38d047b64fb67ba551192c233f01b445cfbe4
jqbk <qibaokan@gmail.com>
2017-10-15 17:40:48 +0800

    finish play4.txt

--- play4/play4.py
+++ play4/play4.py
@@ -3,3 +3,19 @@
 
 # **第 0004 题：**任一个英文的纯文本文件，统计其中的单词出现的个数。
 
+__author__ = 'jqbk'
+__date__ = '2017-06-22'
+
+import re
+
+def count_words():
+    pattern = re.compile(r'\w+')
+
+    with open ('E:\\Software-Docs\\python_documents\\python-exercise\\play4\\article.txt', 'r') as f:
+        words = pattern.findall(f.read())
+        cnt = len(words)
+
+    print('This article have {0:^6d} words.'.format(cnt))
+
+if __name__ == '__main__':
+    count_words()
