ab50cd9e932a6421cde40d2ba3d5a8570a856a41
jqbk <qibaokan@gmail.com>
2017-10-15 10:08:57 +0800

    update play2.py

--- play2/play2.py
+++ play2/play2.py
@@ -10,9 +10,40 @@ import os
 import sys
 import psycopg2
   
-def connectPostgreSQL():
-    conn = psycopg2.connect(database="postgres", user="postgres", password="R011638q", host="127.0.0.1", port="5432")
+def write_coupon():
+    conn = psycopg2.connect(database="testdb", user="jqbk", password="R011638q", host="127.0.0.1", port="5432")
     print('connect successful!')
 
+    cur = conn.cursor()
+    cur.execute('SELECT tablename FROM pg_tables;')
+    tablenames = cur.fetchall()
+
+    # 判断表是否存在
+    found = False
+    if ('coupon',) in tablenames:
+        found = True
+
+    if not found:
+        cur.execute('CREATE TABLE coupon(ID INTEGER, code VARCHAR(10));')
+        print('creat table successfully!')
+    
+    with open('E:\\Software-Docs\\python_documents\\python-exercise\\play1\\coupon.txt', 'r') as f:
+        content = f.readlines()
+    
+    for i in range(len(content)):
+        cur.execute('INSERT INTO coupon(ID, code) \
+                    VALUES(%s, %s);', (i+1, content[i])) # 占位符只能是%s
+    
+    conn.commit()
+
+    cur.execute('SELECT * FROM coupon;')
+    rows = cur.fetchall()
+    for row in rows:
+        print('ID = ', row[0])
+        print('code = ', row[1], '\n')
+    
+    cur.close()
+    conn.close()
+
 if __name__=='__main__':
-    connectPostgreSQL()
+    write_coupon()
