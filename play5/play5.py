72e8d19a533ab837fc1218e9e3eee46d4625d035
jqbk <qibaokan@gmail.com>
2017-10-18 10:43:51 +0800

    update play5.py

--- play5/play5.py
+++ play5/play5.py
@@ -1,8 +1,14 @@
 #! usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-**第 0005 题：**你有一个目录，装了很多照片，把它们的尺寸变成都不大于 iPhone5 分辨率的大小。
+# **第 0005 题：**你有一个目录，装了很多照片，把它们的尺寸变成都不大于 iPhone5 分辨率的大小。
 
 __author__ = 'jqbk'
 __date__ = '2017-06-22'
 
+import os
+from PIL import Image
+
+path = 'E:\\Software-Docs\\python_documents\\python-exercise\\play5\\背景图片\\'
+
+print(os.listdir(path))
