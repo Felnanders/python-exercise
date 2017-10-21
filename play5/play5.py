40ca4356e9c9b7d9f42dc669ae13acf16d5d91a4
jqbk <qibaokan@gmail.com>
2017-10-21 22:26:39 +0800

    finish play5.py

--- play5/play5.py
+++ play5/play5.py
@@ -9,6 +9,23 @@ __date__ = '2017-06-22'
 import os
 from PIL import Image
 
-path = 'E:\\Software-Docs\\python_documents\\python-exercise\\play5\\背景图片\\'
+def change():
+    path = 'E:\\Software-Docs\\python_documents\\python-exercise\\play5\\背景图片\\'
+    os.chdir(path)
+    pics = os.listdir(path)
 
-print(os.listdir(path))
+    for pic in pics:
+        im = Image.open(pic)
+        im = im.convert('RGB')
+        w, h = im.size
+        # pic_format = im.format
+        while w > 640 or h > 1136:
+            w = 640
+            h = 1136
+            im.thumbnail((w, h))
+            pic_newname = pic.split('.')[0] + 'jq.jpg'
+            im.save(pic_newname, 'JPEG')
+    print('All pictures are changed!')
+
+if __name__ == '__main__':
+    change()
